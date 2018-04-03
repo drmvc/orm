3ddbe5d9b696d73422476793c138dc75852460ab
Compolomus <compolom@gmail.com>
2018-04-03 06:32:40 +0200

    Refactoring entity

--- src/DoctorineORM/Entity.php
+++ src/DoctorineORM/Entity.php
@@ -2,16 +2,63 @@
 
 namespace DrMVC\DoctorineORM;
 
-use DrMVC\DoctorineORM\Traits\Magic;
-
 class Entity
 {
-    use Magic;
+
+    const SELECT = 1;
+
+    const UPDATE = 2;
+
+    const INSERT = 3;
+
+    const DELETE = 4;
+
+    private $_flag = self::SELECT;
 
     private $_table;
 
     public function __construct(string $table)
     {
         $this->_table = $table;
+    }
+
+    private $data;
+
+    public function __set(string $name, $value)//: void 7.1
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __get(string $name)
+    {
+        return $this->data[$name] ?? null;
+    }
+
+    public function __isset(string $name): bool
+    {
+        return isset($this->data[$name]);
+    }
+
+    public function __unset(string $name)//: void 7.1
+    {
+        unset($this->data[$name]);
+    }
+
+    public function __call($name, $arguments) {
+        $action = substr($name, 0, 3);
+        $property = strtolower(substr($name, 3));
+        switch ($action) {
+            case 'get':
+                return $this->$property;
+                break;
+
+            case 'set':
+                $this->_flag = isset($this->$property) ? self::UPDATE : self::INSERT;
+                $this->$property = $arguments[0];
+                break;
+
+            default :
+                return false;
+        }
     }
 }
