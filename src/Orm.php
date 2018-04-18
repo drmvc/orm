07cc61ec46d92fa7583605fdaa58258a5a1788a9
Paul Rock <paul@drteam.rocks>
2018-04-18 08:00:27 +0300

    code coverage enabled, test.db moved to memory, small fixes in tests for in-memory databases

--- src/Orm.php
+++ src/Orm.php
@@ -2,12 +2,10 @@
 
 namespace DrMVC;
 
-use DrMVC\Orm\Orm as DrMVCOrm;
-
 /**
  * Class ORM - Abstract class for access to factory
  * @package DrMVC
  */
-class Orm extends DrMVCOrm
+class Orm extends Orm\Orm
 {
 }
